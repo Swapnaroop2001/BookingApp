280da52a544440d17cf1128b8f248dc7fdd89aa0
Swapnaroop <swapnaroop2001@gmail.com>
2024-02-10 16:30:11 -0500

    minor change

--- routes/hotels.js
+++ routes/hotels.js
@@ -76,10 +76,7 @@ router.get('/countByType', async (req, res) => {
 router.get('/', async (req, res, next) => {
     const { min, max, ...others } = req.query;
     try {
-        const hotels = await Hotel.find({
-            ...others,
-            cheapestPrice: { $gt: min | 1, $lt: max || 999 },
-        }).limit(req.query.limit);
+        const hotels = await Hotel.find().limit(req.query.limit);
         res.status(200).json(hotels);
     } catch (err) {
         next(err);
